59573e95980f441b6ad2fe6b8d95e9845aad4c65
Stephen Belyea <s2belyea@gobonfire.com>
2019-12-13 14:18:03 -0500

    Get messages stub, fixes to appointments and templates

--- src/services/appointments.js
+++ src/services/appointments.js
@@ -3,7 +3,7 @@ const data = [
     id: "1",
     patientName: "Sample Patient",
     patientPhone: "123-456-7890",
-    practitionerName: "Sample Practitioner",
+    practitionerName: "Dr. Sample Practitioner",
     clinicName: "Foo Bar Inc.",
     clinicAddress: "123 Fake Street, Kitchener, ON, N1O 1N0",
     clinicPhone: "987-654-3211",
@@ -14,12 +14,12 @@ const data = [
     id: "2",
     patientName: "Patient Sample",
     patientPhone: "123-456-7890",
-    practitionerName: "Practitioner Sample",
+    practitionerName: "Practitioner Sample, RMT",
     clinicName: "Boo Far Co.",
     clinicAddress: "678 Artificial Ave., Kitchener, ON, N1O 1N0",
     clinicPhone: "987-654-3211",
     date: new Date(),
-    confirmed: false
+    confirmed: true
   }
 ];
 
