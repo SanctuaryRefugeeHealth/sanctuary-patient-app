2c2544511753de6c1dfcb8d93c0c3f9c9bbfeb33
jaey p <jp.inseoul@gmail.com>
2020-04-12 03:10:17 -0400

    changed method name

--- src/pages/AppointmentDetails.jsx
+++ src/pages/AppointmentDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getAppointmentById } from "../services/appointments";
+import { getAppointment } from "../services/appointments";
 import { CircularProgress, Typography } from "@material-ui/core/";
 import { useLocation } from "react-router-dom";
 import moment from "moment";
@@ -10,7 +10,7 @@ export default () => {
   useEffect(() => {
     const getData = async () => {
       const appointmentId = location.pathname.split("/").pop();
-      const data = await getAppointmentById(appointmentId);
+      const data = await getAppointment(appointmentId);
       setAppointment(data);
     };
     getData();
