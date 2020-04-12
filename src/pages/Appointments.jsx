35631516bc160c2fb7338b71ae979c34d83a4a80
jaey p <jp.inseoul@gmail.com>
2020-04-12 11:47:46 -0400

    removed unused code

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
-import { APPOINTMENTS } from "../constants/apis";
 import {
   Table,
   TableBody,
