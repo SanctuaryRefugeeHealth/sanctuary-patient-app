f701b8e01850e2912c230839dd1c748836ac5be2
jaey p <jp.inseoul@gmail.com>
2020-04-13 00:55:53 -0400

    added constants for languages

---
+++ src/constants/languages.js
@@ -0,0 +1,40 @@
+const languages = [
+    {
+        id: 1,
+        iso: "en",
+        name: "English",
+        direction: "ltr"
+    },
+    {
+        id: 2,
+        iso: "ar",
+        name: "Arabic",
+        direction: "rtl"
+    },
+    {
+        id: 3,
+        iso: "es",
+        name: "Spanish",
+        direction: "ltr"
+    },
+    {
+        id: 4,
+        iso: "ti",
+        name: "Tigrinya",
+        direction: "ltr"
+    },
+    {
+        id: 5,
+        iso: "so",
+        name: "Somali",
+        direction: "ltr"
+    },
+    {
+        id: 6,
+        iso: "tr",
+        name: "Turkish",
+        direction: "ltr"
+    }
+];
+
+export default languages
