cbfedeb6c7a9d89d541b15fa2fa6e196fd66c152
jaey p <jp.inseoul@gmail.com>
2020-05-06 12:46:09 -0400

    Fixed datetime format error

--- src/pages/NewAppointment.jsx
+++ src/pages/NewAppointment.jsx
@@ -50,7 +50,7 @@ const NewAppointment = () => {
       e.preventDefault();
       const getLanguageById = id => languages[id - 1].name.toLowerCase()
       const language = getLanguageById(formData.patientLanguage)
-      const formattedDate = moment.utc(selectedDate).format('YYYY-MM-DD HHmmss')
+      const formattedDate = moment.utc(selectedDate).format('YYYY-MM-DD HH:mm:ss')
       createAppointment({ ...formData, patientLanguage: language, date: formattedDate })
         .then(handle('moveNext'))
     },
