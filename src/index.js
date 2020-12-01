9e70690d1c8ed92a162d295cf9294e0df3585f62
Sam Lewis <sam@wiles.co>
2020-12-01 09:51:20 +0000

    Removed lint test

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from "./App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-const a = 5;
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
